5f4ad3ad0cb4530e5323b87aa711274e63a9ec4f
JungukHom <developer0223@gmail.com>
2019-05-03 15:52:43 +0900

    Add & Edit | Logger

---
+++ Projects/Logger/Assets/Logger/Demos/Scripts/Test.cs
@@ -0,0 +1,14 @@
+﻿namespace developer0223
+{
+    using UnityEngine;
+
+    using developer0223.Logger;
+
+    public class Test : MonoBehaviour
+    {
+        private void Start()
+        {
+            Log.Write("Hello World");
+        }
+    }
+}
